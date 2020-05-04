a20f405de013e9d3f871e839dcf7826c6553542c
J. Alvarez <jcalvarezj@gmail.com>
2020-05-03 19:17:13 -0500

    Initial data manipulation using pandas for obtaining JSON objects for country records

--- beds.py
+++ beds.py
@@ -4,19 +4,38 @@ class BedsRecord:
     """
     This class represents a record from the beds capacity dataset
     """
-    def __init__(self, code=None, lat=None, lng=None, beds=None,
-                 population=None, source=None, source_url=None, 
-                 bed_type=None, estimated_beds=None):
+    def __init__(self, code=None, lat=None, lng=None, beds_average=None,
+                 population=None, estimated_beds_average=None, source=None,
+                 source_url=None):
         self._code = code
         self._lat = lat
         self._lng = lng
-        self._beds = beds
+        self._beds_average = beds_average
         self._population = population
-        self._estimated_beds = estimated_beds
+        self._estimated_beds_average = estimated_beds_average
         self._source = source
         self._source_url = source_url
-        self._bed_type = bed_type
+        self._bed_type = []
 
 
     def __str__(self):
-        return json.dumps(self.__dict__, indent=4)
+        structure = {
+            'code': self._code,
+            'lat': self._lat,
+            'lng': self._lng,
+            'bedsAverage': self._beds_average,
+            'population': self._population,
+            'estimatedBedsAverage': self._estimated_beds_average,
+            'source': self._source,
+            'sourceUrl': self._source_url,
+            'bedType': self._bed_type
+        }        
+        return json.dumps(structure, indent=4)
+
+    
+    def add_bed_types(self, bed_types):
+        """
+        Adds all the bed_types objects to the instance's bed_type list
+        """
+        for bt in bed_types:
+            self._bed_type.append(bt)
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import os
 from beds import BedsRecord
 from enum import Enum
 import pandas as pd
+import traceback
 
 
-class BED_FILTER_CATEGORIES(Enum):
+class BedsFilter(Enum):
     """
     Enum that provides constant integers to describe category filters for the
     beds dataset
@@ -14,11 +15,13 @@ class BED_FILTER_CATEGORIES(Enum):
     BOTTOM_COUNTRIES_BY_SCALE = 3
     TOP_COUNTRIES_BY_ESTIMATE = 4
     BOTTOM_COUNTRIES_BY_ESTIMATE = 5
+    TOP_COUNTRIES_BY_AVERAGE = 6
+    BOTTOM_COUNTRIES_BY_AVERAGE = 7
 
 
 BEDS_FILENAME = './data/hospital_beds.csv'
 MENU = [
-    '''Please enter the dataset to process or x to exit: 
+    '''Please enter the desired option: 
 
     (1) Global bed capacity
     (2) Measures and restrictions by country
@@ -34,8 +37,7 @@ MENU = [
     (0) Go back''',
     '''\nMeasures dataset chosen. What filter would you like to apply?
 
-    (...) Yet to implement
-    E(x)it'''
+    (...) Yet to implement'''
 ]
 
 
@@ -48,12 +50,28 @@ def prompt_user(message):
 
 
 def filter_beds(category):
+    """
+    Returns the dataset (pandas.core.frame.DataFrame) filtered by the input
+    filter category
+    """
     try:
         data = pd.read_csv(BEDS_FILENAME)
-        print(data)
-        print(f'USING CATEGORY {category}')
+        if (category == BedsFilter.NUMBER_PERCENT_COUNTRY_NORMAL.value):
+            return data
+        elif (category == BedsFilter.TOP_COUNTRIES_BY_SCALE.value):
+            raise Exception("Not implemented yet")
+        elif (category == BedsFilter.BOTTOM_COUNTRIES_BY_SCALE.value):
+            raise Exception("Not implemented yet")
+        elif (category == BedsFilter.TOP_COUNTRIES_BY_ESTIMATE.value):
+            raise Exception("Not implemented yet")
+        elif (category == BedsFilter.BOTTOM_COUNTRIES_BY_ESTIMATE.value):
+            raise Exception("Not implemented yet")
+        elif (category == BedsFilter.NUMBER_PERCENT_COUNTRY_NORMAL.value):
+            raise Exception("Not implemented yet")
     except FileNotFoundError:
         print(f'The file "{BEDS_FILENAME}" does not exist')
+    except Exception as e:
+        print(e)
 
 
 def validate_option(option, min_value, max_value):
@@ -62,6 +80,47 @@ def validate_option(option, min_value, max_value):
     else:
         print('\nNot a valid option! Try again\n')
         return False
+
+
+def process_raw_data(data):
+    records = []
+
+    country_data = data.groupby('country')
+
+    for name, group in country_data:
+        print(name)
+        print('--------')
+        print(group)
+        print('--------\n\n')
+
+        beds_average = group['beds'].mean()
+
+        population = group['population'].mean()
+
+        # print(f'Promedio de camas para {name}: {beds_average}')
+
+        bed_type = group['beds']
+
+        bed_type_count = bed_type.sum()
+
+        # print(f'Los tipos para {name} son\n{bed_type} de total {bed_type_count}')
+
+
+        # print(f'Las coordenadas de {name} son {group.lat[0]} {group.lng[0]}')
+
+        # print(f"CREANDO REGISTRO PARA {name} ... grupo: {group.values[0]}")
+
+        new_record = BedsRecord(code = name.lower(),
+                                lat = float(group.lat.values[0]),
+                                lng = float(group.lng.values[0]),
+                                beds_average = float(beds_average),
+                                population = population,
+                                estimated_beds_average = float(beds_average))
+
+        print(f'MI NUEVO REGISTRO ES!!! ')
+        print(new_record)        
+
+        # records.append(new_record)
 
 
 def main():
@@ -86,11 +145,17 @@ def main():
                             if (filter_option == 0):
                                 filter_navigation = False
                             else:
-                                filter_beds(filter_option)
+                                raw_data = filter_beds(filter_option).head(6)
+
+                                records = process_raw_data(raw_data)
                 else:
-                    print("\n*** YET TO IMPLEMENT ***\n")
+                    raise Exception("Not implemented yet")
         except ValueError:
             print('\nSorry, only numbers are valid! Try again\n')
+        except Exception as e:
+            print("An error happened!")
+            print(e)
+            traceback.print_exc()
 
 
 if __name__ == "__main__":
